3a1934ff8bc77fdf2d9275046b560c7fd79fb984
aji177 <58924493+aji177@users.noreply.github.com>
2020-11-14 12:22:32 +0700

    Revisi Selesai 14 Nov 2020

--- src/pages/antrian/index.js
+++ src/pages/antrian/index.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { Layout, Breadcrumb, Button } from "antd";
-import firebase from "../../firebase";
+import { Layout, Breadcrumb } from "antd";
 
 import TableAntrian from "./table";
 
 const { Content } = Layout;
 
 const Antrian = () => {
-  const handleUpdateAll = async () => {
-    const collection = await firebase.firestore().collection("antrian").get();
-    collection.forEach((doc) => {
-      doc.ref
-        .update({
-          kerusakan: [],
-          success: 0,
-        })
-        .finally(() => console.log(`${doc.id} Reset Success`));
-    });
-  };
-
   return (
     <React.Fragment>
       <Breadcrumb style={{ margin: "24px 16px 0px 16px" }}>
@@ -35,11 +22,6 @@ const Antrian = () => {
           flexWrap: "wrap",
         }}
       >
-        <div style={{ width: "10%", marginBottom: "10px" }}>
-          <Button type="primary" onClick={handleUpdateAll}>
-            Pengerjaan
-          </Button>
-        </div>
         <TableAntrian />
       </Content>
     </React.Fragment>
